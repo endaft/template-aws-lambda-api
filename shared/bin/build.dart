c33fd8629ca3b67bdfcd299d944013cee68061f1
Gio Palacino <gio@palacino.net>
2022-04-06 18:36:05 -0500

    enhance(build): use task sequence loop

--- shared/bin/build.dart
+++ shared/bin/build.dart
@@ -2,25 +2,41 @@ import 'package:endaft/endaft.dart' as endaft;
 
 final logger = endaft.Logger();
 
-Future<void> check() async {
-  await endaft.CheckCommand(logger).run();
+Future<bool> check() async {
+  return endaft.CheckCommand(logger).run();
 }
 
-Future<void> test() async {
-  await endaft.TestCommand(logger).run();
+Future<bool> validate() async {
+  return endaft.CheckCommand(logger).run();
 }
 
-Future<void> build() async {
-  await endaft.BuildCommand(logger).run();
+Future<bool> test() async {
+  return endaft.TestCommand(logger).run();
 }
 
-Future<void> aggregate() async {
-  await endaft.AggregateCommand(logger).run();
+Future<bool> shared() async {
+  return endaft.SharedCommand(logger).run();
+}
+
+Future<bool> docker() async {
+  return endaft.DockerCommand(logger).run();
+}
+
+Future<bool> aggregate() async {
+  return endaft.AggregateCommand(logger).run();
 }
 
 void main() async {
-  await check();
-  await test();
-  await build();
-  await aggregate();
+  List<endaft.WorkerFunc> sequence = [
+    check,
+    validate,
+    test,
+    shared,
+    docker,
+    aggregate,
+  ];
+  for (var task in sequence) {
+    final result = await task();
+    if (!result) break;
+  }
 }
